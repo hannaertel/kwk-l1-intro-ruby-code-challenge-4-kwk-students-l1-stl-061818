bf8a2827bf99258c94d9eebf93586275efbb2550
hannaertel <hannaertel@yahoo.com>
2019-01-02 22:01:50 +0000

    Automatically backed up by Learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,28 @@
 #write out your code here
 
-def least_coins(cents)
+def least_coins(cents = 29)
+quarter = 25
+dime = 10 
+nickel = 5 
+penny = 1 
+coins = {}
+coins = {
+  "quarters" => 25,
+  "dimes" => 10,
+  "nickels" => 5,
+  "pennies" => 1
+}
+final = {}
+coins.each do |key, values|
+ 
+  final[key] = cents/values
+  final.values.sum == 29
+   
+end
+puts final 
+
 
 #Code your answer here!
 
 end
+least_coins
